80f35a9a5ae69b9b0489db154f9f468e57b3c002
0rphanim <stalelyteyka@yahoo.com>
2024-05-28 18:03:36 +0300

    Create Apocrypha.js

---
+++ Apocrypha.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         NLR Minimap
+// @version      2.7.2.4
+// @description  Ave Luna!
+// @author       Endless Night
+// @include      *://pixelplanet.fun/*
+// @include 	 *://fuckyouarkeros.fun/*
+// ==/UserScript==
+//
+// To the glory of Luna and the New Lunar Republic
+//
